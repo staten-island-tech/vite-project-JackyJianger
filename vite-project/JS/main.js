5bb4da1e1858ffefe8d87c88f5e485d9bee3d951
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2024-10-31 12:22:12 -0400

    a

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,1 +1,26 @@
 import '../CSS/style.css';
+const DOMSelectors = {
+    cardContainer: document.getElementById("container-box"),
+}
+const groceryItems = [
+    { item: "Apples", price: 1.50 },
+    { item: "Bananas", price: 0.75 },
+    { item: "Bread", price: 2.00 },
+    { item: "Milk", price: 3.50 },
+    { item: "Eggs", price: 2.25 },
+    { item: "Chicken Breast", price: 5.99 },
+    { item: "Broccoli", price: 1.20 },
+    { item: "Rice", price: 1.80 },
+    { item: "Pasta", price: 1.00 },
+    { item: "Cheese", price: 4.50 }
+];
+function injectCardIntoDOM(groceryItem) {    
+    DOMSelectors.cardContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="itemCards">
+        <p>${groceryItem.item}</p>
+      </div>`
+    );
+  }
+
+groceryItems.forEach((groceryItem) => injectCardIntoDOM(groceryItem));      
